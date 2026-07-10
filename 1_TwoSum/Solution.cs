b0a051cbe3f52492714ac5888d1fc254c7364113
kris10ansn <kristian.nessa@protonmail.com>
2026-07-10 21:32:22 +0200

    perf(1): use hash map for O(n) two sum
    Replace nested loop approach with hash map solution for better
    time complexity. Store each number and its index in a dictionary,
    then check if the complement exists before processing the next
    number. Reduces time complexity from O(n²) to O(n).

--- 1_TwoSum/Solution.cs
+++ 1_TwoSum/Solution.cs
@@ -7,19 +7,19 @@ public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
+        Dictionary<int, int> map = new();
+
         for (int i = 0; i < nums.Length; i++)
         {
             int a = nums[i];
+            int complement = target - a;
 
-            for (int j = i + 1; j < nums.Length; j++)
+            if (map.TryGetValue(complement, out int complementIndex))
             {
-                int b = nums[j];
-
-                if (a + b == target)
-                {
-                    return new int[] { i, j };
-                }
+                return [i, complementIndex];
             }
+
+            map.TryAdd(a, i);
         }
 
         throw new ArgumentException("The provided array does not have a two sum");
